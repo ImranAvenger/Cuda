afc634b950916a705c000af9d3d7f2c9eb091c78
ImranAvenger <ImranAvenger@users.noreply.github.com>
2020-11-01 23:44:35 +0600

    Updated

--- resources/js/main.js
+++ resources/js/main.js
@@ -15,3 +15,10 @@ $(document).ready(function(){
     var mixer = mixitup('.container');
 });
 
+function openNav() {
+    document.getElementById("myNav").style.width = "100%"
+}
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%"
+}
+
