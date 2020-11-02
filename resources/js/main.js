67856cfc2b82d0bd0f76578802b44a79b3623948
ImranAvenger <ImranAvenger@users.noreply.github.com>
2020-11-03 00:19:03 +0600

    updated

--- resources/js/main.js
+++ resources/js/main.js
@@ -21,4 +21,17 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.width = "0%"
 }
+// SMOOTH SCROLL FOR SAFARI/IE/EDGE 
 
+$("a").on('click', function(event){
+    if (this.hash !== ""){
+        event.preventDefault();
+        
+        var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top}, 800, function(){
+            window.location.hash = hash;
+        
+        });
+        }
+});
